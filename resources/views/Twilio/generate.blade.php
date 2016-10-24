9cb92ebd454d9973358b0a37b75e67ec1006d341
tier5 <github@tier5.us>
2016-10-24 17:02:52 +0530

    call version 1.2

--- resources/views/Twilio/generate.blade.php
+++ resources/views/Twilio/generate.blade.php
@@ -1,8 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <Response>
 	
-    <Gather action="{{ config('app.url') . "/phone/check-confirmation/{$BusinessListing->id}" }}">
-        <Play>https://api.twilio.com/cowbell.mp3</Play>
+    <Play>https://api.twilio.com/cowbell.mp3</Play>
 
-    </Gather>
+    
 </Response>
