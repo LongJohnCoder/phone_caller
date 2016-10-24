a88c304996d62624eb84584a38e4e563f4136fd4
tier5 <github@tier5.us>
2016-10-24 17:58:27 +0530

    checking call

--- app/Http/Controllers/DirectoryController.php
+++ app/Http/Controllers/DirectoryController.php
@@ -96,7 +96,7 @@ class DirectoryController extends Controller
         
     }
     public function readNCall($direct){
-            $fpath=url('/')."/public/phonexml/9935.xml";
+            $fpath=url('/')."/directory/calllist/".$direct;
             //dd($fpath);
             $sid = env('TWILLIO_LIVE_SID');
             $token = env('TWILLIO_LIVE_TOKEN');
