ff01b8becbda83e7d1fc1b97764daa0f8a8f41bc
tier5 <github@tier5.us>
2016-10-26 13:05:58 +0530

    call version V1.03

--- resources/views/Twilio/generate.blade.php
+++ resources/views/Twilio/generate.blade.php
@@ -3,6 +3,7 @@
 	
     <Gather action="{{ config('app.url') . "/phone/check-confirmation/{$value->id}" }}">
         <Play loop="10">{{$fpath}}</Play>
+        <say>Please press 0 to disconnect or 1 to call now or give us a prefer time to call in 24 hundrade format</say>
         
     </Gather>
 </Response>
