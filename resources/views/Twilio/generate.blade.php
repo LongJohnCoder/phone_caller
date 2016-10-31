2154072ace472107f583aa77e53b1e5fda72ee61
tier5 <github@tier5.us>
2016-10-31 12:49:14 +0530

    call version 2.05

--- resources/views/Twilio/generate.blade.php
+++ resources/views/Twilio/generate.blade.php
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <Response>
-	<Play loop="1">{{$fpath}}</Play>
-    <Gather action="{{ $location}}" finishOnKey="*" >
+	
+    <Gather action="{{ $location}}" method="GET" >
         
         <Say>Please press 0 to disconnect or 1 to call now or give us a prefer time to call in 24 hundrade format and then press star</Say>
         
